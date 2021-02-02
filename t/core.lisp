48784bff63ccb4040b2b476e4627c1f8ac99f594
Alexander Artemenko <svetlyak.40wt@gmail.com>
2021-02-02 23:57:14 +0300

    Custom tests runner.

--- t/core.lisp
+++ t/core.lisp
@@ -6,7 +6,11 @@
 (in-package cl-info-test/core)
 
 
+(defun foo (a b)
+  (list a b 3))
+
+
 (deftest test-some-staff
-    (testing "Replace this test with real staff."
-      (assert-that (foo 1 2)
-                   (contains 1 2))))
+  (testing "Replace this test with real staff."
+    (assert-that (foo 1 2)
+                 (contains 1 2))))
